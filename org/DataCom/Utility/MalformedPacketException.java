59312ad0982c5b19bd05edb51daa82b8d1045b99
Jordon Biondo <biondoj@mail.gvsu.edu>
2013-04-05 14:41:27 -0400

    String constructor for malformedpacketexception

--- org/DataCom/Utility/MalformedPacketException.java
+++ org/DataCom/Utility/MalformedPacketException.java
@@ -5,5 +5,8 @@ package org.DataCom.Utility;
  * Exception thrown while parsing packets
  */
 public class MalformedPacketException extends Exception {
-
+    
+    public MalformedPacketException(String s) {
+	super(s);
+    }
 }
