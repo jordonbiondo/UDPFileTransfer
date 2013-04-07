8934cf775eca037cd72bd41957004208c41ec684
Jason Tierney <jason.tierney@live.com>
2013-04-06 23:13:35 -0400

    Compiler for Windows
    You can use this to compile the project if you're running Windows.

---
+++ compile-win.bat
@@ -0,0 +1,3 @@
+ECHO OFF
+javac C:/Users/Jason/Documents/GitHub/UDPFileTransfer/org/DataCom/Client/*.java C:/Users/Jason/Documents/GitHub/UDPFileTransfer/org/DataCom/Server/*.java C:/Users/Jason/Documents/GitHub/UDPFileTransfer/org/DataCom/Testing/*.java C:/Users/Jason/Documents/GitHub/UDPFileTransfer/org/DataCom/Utility/*.java
+ECHO ON
